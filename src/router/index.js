8e02792624efff322e1f6f5cf8fee13a94ff3c2f
kimura121212 <ykimuranoi@gmail.com>
2019-11-30 18:55:38 +0900

    Login画面作成

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Login from './views/Login.vue'
+import Login from '../views/Login.vue'
 import ChatDashboard from '../views/ChatDashboard.vue'
 
 Vue.use(VueRouter)
--- src/store/index.js
+++ src/store/index.js
@@ -14,11 +14,25 @@ const vuexLocal = new VuexPersistence({
 
 export default new Vuex.Store({
   state: {
+    loading: false,
+    sending: false,
+    error: null,
+    user: [],
+    reconnect: false,
+    activeRoom: null,
+    rooms: [],
+    users: [],
+    messages: [],
+    userTyping: null
   },
-  mutations,
-  actions,
-  getters: {
+  mutations: {
 
+  },
+  actions: {
+
+  },
+  getters: {
+    hasError: state => state.error ? true : false
   },
   plugins: [vuexLocal.plugin],
   strict: debug
