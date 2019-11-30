f095976ce9b6956d258e725edf5b5a15a9e12cf6
kimura121212 <ykimuranoi@gmail.com>
2019-11-30 20:16:58 +0900

    change room

--- src/store/actions.js
+++ src/store/actions.js
@@ -21,10 +21,34 @@ export default {
 
       // Test state.user
       console.log(state.user);
+      const rooms = currentUser.rooms.map(room => ({
+        id: room.id,
+        name: room.name
+      }))
+      commit('setRooms', rooms);
+
+      const activeRoom = state.activeRoom || rooms[0];
+
+      commit('setActiveRoom', {
+        id: activeRoom.id,
+        name: activeRoom.name
+      }),
+
+      await chatkit.subscribeToRoom(activeRoom.id);
+
+      return true
     } catch (error) {
       handleError(commit, error)
     } finally {
       commit('setLoading', false);
+    }
+  },
+  async changeRoom({ commit }, roomId) {
+    try {
+      const { id, name } = await chatkit.subscribeToRoom(roomId);
+      commit('setActiveRoom', {id, name});
+    }catch(error){
+      handleError(commit, error)
     }
   }
 }
